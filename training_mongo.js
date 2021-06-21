b3b66061262cd7ef3de4b2c8807fe4ee48bb3888
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-21 14:05:44 +0300

    Initial commit

---
+++ training_mongo.js
@@ -0,0 +1,13 @@
+db.getCollection('teacher').find({
+    $or:[
+    {payment:2500},
+    {payment:2200},
+    {class_curator: 6}
+    ]},
+        {
+        name: 1,
+        payment: 1,
+     }
+    )
+
+
